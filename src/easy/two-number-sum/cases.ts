fad31dc4e50bdcff8c063ffd9b3d5dd8249868cd
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-15 13:32:32 -0300

    refactor(easy): two numbers sum tests

---
+++ src/easy/two-number-sum/cases.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    array: [3, 5, -4, 8, 11, 1, -1, 6],
+    targetSum: 10,
+    expected: [11, -1]
+  },
+  {
+    array: [4, 6],
+    targetSum: 10,
+    expected: [4, 6]
+  },
+  {
+    array: [4, 6, 1],
+    targetSum: 5,
+    expected: [4, 1]
+  },
+  {
+    array: [4, 6, 1, -3],
+    targetSum: 3,
+    expected: [6, -3]
+  },
+  {
+    array: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    targetSum: 17,
+    expected: [8, 9]
+  },
+  {
+    array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
+    targetSum: 18,
+    expected: [3, 15]
+  },
+  {
+    array: [-7, -5, -3, -1, 0, 1, 3, 5, 7],
+    targetSum: -5,
+    expected: [-5, 0]
+  },
+  {
+    array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
+    targetSum: 163,
+    expected: [210, -47]
+  },
+  {
+    array: [-21, 301, 12, 4, 65, 56, 210, 356, 9, -47],
+    targetSum: 164,
+    expected: []
+  },
+  {
+    array: [3, 5, -4, 8, 11, 1, -1, 6],
+    targetSum: 15,
+    expected: []
+  },
+  {
+    array: [14],
+    targetSum: 15,
+    expected: []
+  },
+  {
+    array: [15],
+    targetSum: 15,
+    expected: []
+  }
+];
