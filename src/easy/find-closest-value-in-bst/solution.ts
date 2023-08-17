6d71c535b9ff21e463bc3957c600f9356324b0ae
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-17 08:48:59 -0300

    feat(easy): improve find closest value in bst solution

--- src/easy/find-closest-value-in-bst/solution.ts
+++ src/easy/find-closest-value-in-bst/solution.ts
@@ -43,6 +43,7 @@ class BST {
 function walk(tree: BST | null, target: number, min: number): number {
   if (!tree) return min;
   if (Math.abs(tree.value - target) < Math.abs(min - target)) min = tree.value;
+  if (min === target) return min;
 
   if (target < tree.value) return walk(tree.left, target, min);
   return walk(tree.right, target, min);
@@ -52,7 +53,7 @@ function findClosestValueInBst(tree: BST, target: number) {
   return mySolution1(tree, target);
 }
 
-// Complexity: time O() | space O()
+// Complexity (worst-case): time O(n) | space O(n)
 function mySolution1(tree: BST, target: number) {
   return walk(tree, target, tree.value);
 }
