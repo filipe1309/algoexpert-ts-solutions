dba7f494e45279455b7e09c124ed6e9392c7b451
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-17 18:15:54 -0300

    feat(easy): add my solution to node depths

---
+++ src/easy/node-depths/solution.ts
@@ -0,0 +1,62 @@
+// https://www.algoexpert.io/questions/node-depths
+
+/*
+  Node Depths
+
+  The distance between a node in a Binary Tree and the tree's root is called the
+  node's depth.
+
+  Write a function that takes in a Binary Tree and returns the sum of its nodes'
+  depths.
+
+  Each BinaryTree node has an integer value, a left child node, and a right
+  child node. Children nodes can either be BinaryTree nodes themselves or
+  None / null.
+
+  Sample Input
+  tree =     1
+          /     \
+        2       3
+      /   \    /   \
+    4     5  6     7
+  /   \  /
+  8    9 10
+
+  Sample Output
+  16
+  // The depth of the node with value 2 is 1.
+  // The depth of the node with value 3 is 1.
+  // The depth of the node with value 4 is 2.
+  // The depth of the node with value 5 is 2.
+  // Etc..
+  // Summing all of these depths yields 16.
+*/
+
+class BinaryTree {
+  value: number;
+  left: BinaryTree | null;
+  right: BinaryTree | null;
+
+  constructor(value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function nodeDepths(root: BinaryTree) {
+  return mySolution1(root);
+}
+
+function walk(node: BinaryTree | null, depth: number): number {
+  if (node === null) return 0;
+
+  return depth + walk(node.left, depth + 1) + walk(node.right, depth + 1);
+}
+
+// Complexity (worst-case): time O(n) | space O(n)
+function mySolution1(root: BinaryTree) {
+  return walk(root, 0);
+}
+
+export default nodeDepths;
