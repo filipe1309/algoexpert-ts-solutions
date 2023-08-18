5eaa07a42030afafc7c2c200c1305ed475804aeb
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-18 09:46:37 -0300

    feat(easy): add interative solution to node depths

--- src/easy/node-depths/solution.ts
+++ src/easy/node-depths/solution.ts
@@ -50,13 +50,29 @@ function nodeDepths(root: BinaryTree) {
 
 function walk(node: BinaryTree | null, depth: number): number {
   if (node === null) return 0;
-
   return depth + walk(node.left, depth + 1) + walk(node.right, depth + 1);
 }
 
-// Complexity (worst-case): time O(n) | space O(n)
+// Complexity (worst-case): time O(n) | space O(h) h=height
 function mySolution1(root: BinaryTree) {
   return walk(root, 0);
+}
+
+// Iterative | Stack
+// Complexity (worst-case): time O(n) | space O(h) h=height
+function solution1(root: BinaryTree) {
+  let acc = 0;
+  let stack = [{node: root, depth: 0}];
+  while (stack.length) {
+    let nodeInfo = stack.pop();
+    const {node, depth} = nodeInfo;
+    if (!node) continue;
+    acc += depth;
+    stack.push({node: node.left, depth: depth+1});
+    stack.push({node: node.right, depth: depth+1});
+  }
+
+  return acc;
 }
 
 export default nodeDepths;
