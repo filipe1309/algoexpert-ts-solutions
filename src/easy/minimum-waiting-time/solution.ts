1d6e41745dc3bb56ae58b25f18b909306f960e15
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-20 15:30:15 -0300

    feat(easy): add solution to minimum waiting time

--- src/easy/minimum-waiting-time/solution.ts
+++ src/easy/minimum-waiting-time/solution.ts
@@ -36,7 +36,7 @@
 */
 
 function minimumWaitingTime(queries: number[]) {
-  return mySolution1(queries);
+  return solution1(queries);
 }
 
 // Complexity (worst-case): time O(n^2) | space O(n)
@@ -45,6 +45,15 @@ function mySolution1(queries: number[]) {
   return queries.map(( _, idx) => {
     return queries.slice(0, idx).reduce((prev, curr) => prev + curr, 0);
   }).reduce((prev, curr) => prev + curr);
+}
+
+// Complexity (worst-case): time O(n*logn) | space O(1)
+function solution1(queries: number[]) {
+  queries.sort((a, b) => a - b);
+  return queries.reduce((prev, curr, currIdx) => {
+    let queriesLeft = queries.length - (currIdx + 1);
+    return prev + curr*queriesLeft;
+  }, 0);
 }
 
 export default minimumWaitingTime;
