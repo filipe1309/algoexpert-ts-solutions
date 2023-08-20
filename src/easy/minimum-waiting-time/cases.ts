4b363b6071296fd361a2650910ee7454676de7f0
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-20 11:36:03 -0300

    feat(easy): add my solution to minimum-waiting-time

---
+++ src/easy/minimum-waiting-time/cases.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    input: [3, 2, 1, 2, 6],
+    expected: 17
+  },
+  {
+    input: [2,1,1,1],
+    expected: 6,
+  },
+  {
+    input: [1,2,4,5,2,1],
+    expected: 23,
+  },
+  {
+    input: [25,30,2,1],
+    expected: 32
+  },
+  {
+    input: [1,1,1,1,1],
+    expected: 10
+  },
+  {
+    input: [7,9,2,3],
+    expected: 19
+  },
+  {
+    input: [4,3,1,1,3,2,1,8],
+    expected: 45
+  },
+  {
+    input: [2],
+    expected: 0
+  },
+  {
+    input: [7],
+    expected: 0
+  },
+  {
+    input: [8,9],
+    expected: 8
+  },
+  {
+    input: [1,9],
+    expected: 1
+  },
+  {
+    input: [5,4,3,2,1],
+    expected: 20
+  },
+  {
+    input: [1,2,3,4,5],
+    expected: 20
+  },
+  {
+    input: [1,1,1,4,5,6,8,1,1,2,1],
+    expected: 81
+  },
+  {
+    input: [17,4,3],
+    expected: 10
+  },
+];
