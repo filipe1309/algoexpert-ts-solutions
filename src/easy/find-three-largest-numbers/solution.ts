eb564a4f05de51e8fc3ac1abe50ec2b17ac5f66f
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-27 11:49:09 -0300

    feat(find-three-largest-numbers): add solution

--- src/easy/find-three-largest-numbers/solution.ts
+++ src/easy/find-three-largest-numbers/solution.ts
@@ -1,10 +1,11 @@
 // Test: make test t=find-three-largest-numbers
-function findThreeLargestNumbers(input: number[]) {
-  return mySolution1(input); // time O() | space O()
+function findThreeLargestNumbers(input: number[]): number[] {
+  // return mySolution1(input); // time O(n) | space O(1)
+  return solution1(input); // time O(n) | space O(1)
 }
 
-// Complexity (worst-case): time O() | space O()
-function mySolution1(input: number[]) {
+// Complexity (worst-case): time O(n) | space O(1)
+function mySolution1(input: number[]): number[] {
   let result = [input[0], input[1], input[2]];
 
   for (let i = 3; i < input.length; i++) {
@@ -18,6 +19,30 @@ function mySolution1(input: number[]) {
   }
 
   return result.sort((a, b) => a - b);
+}
+
+// Complexity (worst-case): time O(n) | space O(1)
+function solution1(input: number[]): number[] {
+  let threeLargest = [-Infinity, -Infinity, -Infinity];
+
+  for (let num of input) {
+    updateLargest(threeLargest, num);
+  }
+
+  return threeLargest;
+}
+
+function updateLargest(threeLargest: number[], num: number) {
+  if (threeLargest[2] < num) shiftAndUpdate(threeLargest, num, 2)
+  else if (threeLargest[1] < num) shiftAndUpdate(threeLargest, num, 1)
+  else if (threeLargest[0] < num) shiftAndUpdate(threeLargest, num, 0)
+}
+
+function shiftAndUpdate(array: number[], num: number, idx: number) {
+  for (let i = 0; i <= idx; i++) {
+    if (i === idx) array[i] = num;
+    else array[i] = array[i + 1];
+  }
 }
 
 export default findThreeLargestNumbers;
