840ef402bde0cb636f2850d539ec8a1d17954d93
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-29 11:48:59 -0300

    feat(palindrome-check): add my second solution

--- src/easy/palindrome-check/solution.ts
+++ src/easy/palindrome-check/solution.ts
@@ -1,12 +1,22 @@
 // Test: make test t=palindrome-check
 function palindromeCheck(string: string): boolean {
-  return mySolution1(string); // time O(n) | space O(1)
+  // return mySolution1(string); // time O(n) | space O(1)
+  return mySolution2(string); // time O(n) | space O(1)
 }
 
 // Complexity (worst-case): time O(n) | space O(1)
 function mySolution1(string: string): boolean {
   let reverseString = string.split('').reverse().join('');
   return string === reverseString;
+}
+
+// Complexity (worst-case): time O(n) | space O(1)
+function mySolution2(string: string): boolean {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== string[string.length - 1 - i]) return false;
+  }
+
+  return true;
 }
 
 export default palindromeCheck;
