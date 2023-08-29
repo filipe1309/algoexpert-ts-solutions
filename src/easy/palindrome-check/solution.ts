071dbaff458b9570505bdb837b6b02ddafc5ba45
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-29 13:57:59 -0300

    feat(palindrome-check): add solution with recursion

--- src/easy/palindrome-check/solution.ts
+++ src/easy/palindrome-check/solution.ts
@@ -1,10 +1,12 @@
 // Test: make test t=palindrome-check
 function palindromeCheck(string: string): boolean {
-  return solution1(string); // time O(n^2) | space O(n)
+  // return solution1(string); // String concat // time O(n^2) | space O(n)
+  // return solution2(string); // Recursive // time O(n) | space O(n)
   // return mySolution1(string); // time O(n) | space O(n)
-  // return mySolution2(string); // time O(n) | space O(1)
+  return mySolution2(string); // time O(n) | space O(1)
 }
 
+// Array append
 // Complexity (worst-case): time O(n) | space O(n)
 function mySolution1(string: string): boolean {
   let reverseString = string.split('').reverse().join('');
@@ -20,6 +22,7 @@ function mySolution2(string: string): boolean {
   return true;
 }
 
+// String concatenation
 // Complexity (worst-case): time O(n^2) | space O(1)
 // time (n^2) because string concatenation creates a new string 
 function solution1(string: string): boolean {
@@ -29,6 +32,14 @@ function solution1(string: string): boolean {
   }
 
   return string === reverseString;
+}
+
+// Recursive
+// Complexity (worst-case): time O(n) | space O(n)
+function solution2(string: string, i: number = 0): boolean {
+  let j = string.length - 1 - i;
+  if (j <= i) return true;
+  return (string[i] === string[j]) && solution2(string, i + 1)
 }
 
 export default palindromeCheck;
