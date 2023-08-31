407c3f195ff43b9290a96c8d7e760710b325e502
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-31 20:51:09 -0300

    feat(generate-document): add 2nd solution with count and set

--- src/easy/generate-document/solution.ts
+++ src/easy/generate-document/solution.ts
@@ -1,7 +1,8 @@
 // Test: make test t=generate-document
 function generateDocument(characters: string, document: string) {
   // return mySolution1(characters, document); // time O(m * n) | space O(1) 
-  return solution1(characters, document); // time O(m * (n + m)) | space O(1)
+  // return solution1(characters, document); // time O(m * (n + m)) | space O(1)
+  return solution2(characters, document); // time 
 }
 
 // Complexity (worst-case): time O(m * n) | space O(1)
@@ -17,19 +18,33 @@ function mySolution1(characters: string, document: string) {
 // m = document.length, n = characters.length
 function solution1(characters: string, document: string) {
   for (let char of document) {
-    let documentFrequency = solution1CountCharFrequency(char, document);
-    let charactersFrequency = solution1CountCharFrequency(char, characters);
+    let documentFrequency = countCharFrequency(char, document);
+    let charactersFrequency = countCharFrequency(char, characters);
     if (documentFrequency > charactersFrequency) return false;
   }
   return true;
 }
 
-function solution1CountCharFrequency(targetChar: string, targetString: string): number {
+function countCharFrequency(targetChar: string, targetString: string): number {
   let frequency = 0;
   for (let char of targetString) {
     if (targetChar === char) frequency++;
   }
   return frequency;
+}
+
+// Complexity (worst-case): time O(c * (m + n)) | space O(c)
+// m = document.length, n = characters.length, c = unique characters in document
+function solution2(characters: string, document: string) {
+  const alreadyCount = new Set<string>();
+  for (let char of document) {
+    if (alreadyCount.has(char)) continue;
+    let documentFrequency = countCharFrequency(char, document);
+    let charactersFrequency = countCharFrequency(char, characters);
+    if (documentFrequency > charactersFrequency) return false;
+    alreadyCount.add(char);
+  }
+  return true;
 }
 
 export default generateDocument;
