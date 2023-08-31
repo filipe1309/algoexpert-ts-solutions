e6e0ab459ab60b04941f4bc58639a586f882a7aa
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-31 15:15:47 -0300

    feat(common-characters): add first solution with Set

--- src/easy/common-characters/solution.ts
+++ src/easy/common-characters/solution.ts
@@ -1,9 +1,12 @@
 // Test: make test t=common-characters
 function commonCharacters(strings: string[]): string[] {
-  return mySolution1(strings); // time O(n) | space O(n)
+  // return mySolution1(strings); // time O(n*m) | space O(c)
+  return solution1(strings); // time O(n*m) | space O(c)
 }
 
-// Complexity (worst-case): time O(n) | space O(n)
+// Complexity (worst-case): time O(n*m) | space O(c)
+// n = number of strings, m = length of longest string, 
+// c = number of common characters
 function mySolution1(strings: string[]): string[] {
   let hash: {[key: string]: boolean[]} = {};
   for (let i = 0; i < strings.length; i++) {
@@ -16,6 +19,26 @@ function mySolution1(strings: string[]): string[] {
   let result = [];
   for (let char in hash) {
     if (hash[char].reduce((prev, curr) => prev && curr)) result.push(char);
+  }
+  return result;
+}
+
+// Complexity (worst-case): time O(n*m) | space O(c)
+// n = number of strings, m = length of longest string, 
+// c = number of common characters
+function solution1(strings: string[]): string[] {
+  let charCount: {[key: string]: number} = {};
+  for (let string of strings) {
+    let uniqueChars = new Set(string)
+    uniqueChars.forEach((char) => {
+      if (!charCount[char]) charCount[char] = 0;
+      charCount[char]++;
+    });
+  }
+
+  let result = [];
+  for (let char in charCount) {
+    if (charCount[char] === strings.length) result.push(char);
   }
   return result;
 }
