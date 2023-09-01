aea7b121f3a29e0da6a5ba22fd668a3ed295481d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-01 19:38:15 -0300

    feat(first-non-repeating-character): add solution 2

--- src/easy/first-non-repeating-character/solution.ts
+++ src/easy/first-non-repeating-character/solution.ts
@@ -1,7 +1,8 @@
 // Test: make test t=first-non-repeating-character
 function firstNonRepeatingCharacter(string: string): number {
-  return mySolution1(string); // time O(n) | space O(1)
+  // return mySolution1(string); // time O(n) | space O(1)
   // return mySolution2(string); // time O(n^2) | space O(1)
+  return solution2(string); // time O(n) | space O(1)
 }
 
 // Complexity (worst-case): time O(n) | space O(1)
@@ -13,13 +14,13 @@ function mySolution1(string: string): number {
     else charsMap.set(char, -1);
   }
 
-  let minIndex = -1;
   for (let [key] of charsMap) {
     let charIndex = charsMap.get(key);
+    // it works because Map keeps the order
     if (charIndex !== -1 && charIndex !== undefined) return charIndex;
   }
 
-  return minIndex;
+  return -1;
 }
 
 // Complexity (worst-case): time O(n^2) | space O(1)
@@ -33,6 +34,20 @@ function mySolution2(string: string): number {
       }
     }
     if (!hasMore) return i;
+  }
+
+  return -1;
+}
+
+// Complexity (worst-case): time O(n) | space O(1)
+function solution2(string: string): number {
+  const charsFreqMap = new Map<String, number>();
+  for (let char of string) {
+    charsFreqMap.set(char, (charsFreqMap.get(char) ?? 0) + 1);
+  }
+
+  for (let charIdx = 0; charIdx < string.length; charIdx++) {
+    if (charsFreqMap.get(string[charIdx]) === 1) return charIdx;
   }
 
   return -1;
