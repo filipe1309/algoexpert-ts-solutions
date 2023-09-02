6475462deaa288f7c9dbb7773bdc5df82594c567
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-02 11:53:33 -0300

    feat(semordnilap): add my 2nd solution with Set

--- src/easy/semordnilap/solution.ts
+++ src/easy/semordnilap/solution.ts
@@ -1,9 +1,10 @@
 // Test: make test t=semordnilap
 function semordnilap(words: string[]) {
-  return mySolution1(words); // time O() | space O()
+  // return mySolution1(words); // time O((n * m) ^ 2), | space O(n * m)
+  return mySolution2(words); // time O(n * m), | space O(n * m)
 }
 
-// Complexity (worst-case): time O() | space O()
+// Complexity (worst-case): time O((n * m) ^ 2), | space O(n * m)
 function mySolution1(words: string[]) {
   let semordnilaps = [];
   for (let i = 0; i < words.length; i++) {
@@ -12,7 +13,21 @@ function mySolution1(words: string[]) {
       if (word1Reverse === words[j]) semordnilaps.push([words[i], words[j]])
     }
   }
-  return semordnilaps
+  return semordnilaps;
+}
+
+// Complexity (worst-case): time O(n * m), | space O(n * m)
+function mySolution2(words: string[]) {
+  let wordsSet = new Set(words);
+  let semordnilaps = [];
+  for (let i = 0; i < words.length; i++) {
+    let word1Reverse = words[i].split('').reverse().join('');
+      if (wordsSet.has(word1Reverse) && words[i] !== word1Reverse) {
+        semordnilaps.push([words[i], word1Reverse]);
+        wordsSet.delete(words[i]);
+      }
+  }
+  return semordnilaps;
 }
 
 export default semordnilap;
