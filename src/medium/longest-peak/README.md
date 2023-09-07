5dfe6937cc49240621e8702feb84aa2fd76a672e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-07 11:15:00 -0300

    feat(longest-peak): add 1st solution w/ tip approach

--- src/medium/longest-peak/README.md
+++ src/medium/longest-peak/README.md
@@ -25,3 +25,33 @@ array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
 ```javascript
 6 // 0, 10, 6, 5, -1, -3
 ```
+
+## Hints
+
+<details>
+<summary>Hint 1</summary>
+You can solve this question by iterating through the array from left to right
+once.
+</details>
+
+<details>
+<summary>Hint 2</summary>
+Iterate through the array from left to right, and treat every integer as the
+potential tip of a peak. To be the tip of a peak, an integer has to be strictly
+greater than its adjacent integers.
+</details>
+
+<details>
+<summary>Hint 3</summary>
+As you iterate through the array from left to right, whenever you find a tip of
+a peak, expand outwards from the tip until you no longer have a peak. Given what
+peaks look like and how many peaks can therefore fit in an array, realize that
+this process results in a linear-time algorithm. Make sure to keep track of the
+longest peak you find as you iterate through the array.
+</details>
+
+<details>
+<summary>Optimal Space &amp; Time Complexity</summary>
+O(n) time | O(1) space - where n is the length of the input array
+</details>
+
--- src/medium/longest-peak/solution.ts
+++ src/medium/longest-peak/solution.ts
@@ -1,6 +1,7 @@
 // Test: make test t=longest-peak
 function longestPeak(array: number[]): number {
-  return mySolution1(array); // time O(n^2) | space O(1)
+  // return mySolution1(array); // time O(n^2) | space O(1)
+  return solution1(array); // time O(n) | space O(1)
 }
 
 // Complexity (worst-case): time O(n^2) | space O(1)
@@ -25,5 +26,34 @@ function mySolution1(array: number[]): number {
   }
   return maxCount;
 }
+
+// Tip of a Peak approach
+// 1 - Find tips
+// 2 - Expand left and right
+// 3 - Calculate length
+// 4 - Update max length
+// 5 - Repeat until end of the array
+// Complexity (worst-case): time O(n) | space O(1)
+function solution1(array: number[]): number {
+  let longestPeakLength = 0;
+  for (let i = 1; i < array.length - 1; i++) {
+    if (isATipOfPeak(array, i)) {
+      let leftIdx = i;
+      let rightIdx = i;
+      while (leftIdx >= 0 && array[leftIdx] > array[leftIdx - 1]) { leftIdx--; }
+      while (rightIdx < array.length && array[rightIdx] > array[rightIdx + 1]) { rightIdx++; }
+      let peakLength = rightIdx - leftIdx + 1;
+      longestPeakLength = Math.max(longestPeakLength, peakLength);
+      i = rightIdx
+    }
+  }
+
+  return longestPeakLength;
+}
+
+function isATipOfPeak(array: number[], i: number): boolean {
+  return array[i] > array[i - 1] && array[i] > array[i + 1];
+}
+
 
 export default longestPeak;
