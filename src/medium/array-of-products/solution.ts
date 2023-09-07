2a0c97e8e824b553dcf4f53d775b2c44814b8f60
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-07 14:53:06 -0300

    feat(array-of-products): add 2nd solution w/ left/right

--- src/medium/array-of-products/solution.ts
+++ src/medium/array-of-products/solution.ts
@@ -1,7 +1,8 @@
 // Test: make test t=array-of-products
 function arrayOfProducts(array: number[]): number[] {
   // return mySolution1(array); // time O(n^2) | space O(n)
-  return solution1(array); // time O(n^2) | space O(n)
+  // return solution1(array); // time O(n^2) | space O(n)
+  return solution2(array); // time O(n) | space O(n)
 }
 
 // Brute force approach
@@ -28,6 +29,31 @@ function solution1(array: number[]): number[] {
     }
     products[i] =  runningProduct;
   }
+  return products;
+}
+
+// Left/Right approach
+// Complexity (worst-case): time O(n^2) | space O(n)
+function solution2(array: number[]): number[] {
+  let products: number[] = Array(array.length).fill(1);
+  let leftProducts: number[] = Array(array.length).fill(1);
+  let rightProducts: number[] = Array(array.length).fill(1);
+  let leftRunningProducts = 1;
+  for (let i = 0; i < array.length; i++) {
+    leftProducts[i] = leftRunningProducts
+    leftRunningProducts *= array[i];
+  }
+
+  let rightRunningProducts = 1;
+  for (let i = array.length - 1; i >= 0; i--) {
+    rightProducts[i] = rightRunningProducts
+    rightRunningProducts *= array[i];
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    products[i] = leftProducts[i] * rightProducts[i];
+  }
+
   return products;
 }
 
