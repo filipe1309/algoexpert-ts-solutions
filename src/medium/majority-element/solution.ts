de0f4140deef7d1bb3336eeefce38698d22a1bce
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-11 19:23:22 -0300

    feat(majority-element): add 2st solution w/ bitwise

--- src/medium/majority-element/solution.ts
+++ src/medium/majority-element/solution.ts
@@ -1,7 +1,9 @@
 // Test: make test t=majority-element
 function majorityElement(array: number[]): number {
   // return mySolution1(array); // time O(nlogn) | space O(1)
-  return solution1(array); // time O(nlogn) | space O(1)
+  return solution1(array); // time O(n) | space O(1)
+  // return solution2(array); // time O(n) | space O(1)
+  // return solution3(array); // time O(n) | space O(1)
 }
 
 // Sorting approach
@@ -20,6 +22,44 @@ function solution1(array: number[]): number {
     if (count === 0) answer = num;
     if (num === answer) count++;
     else count--;
+  }
+
+  return answer;
+}
+
+// Bitwise approach
+// Complexity (worst-case): time O(n) | space O(1)
+// Explanation: 
+// 1. We iterate through each bit of the number
+// 2. We count the number of 1s in that bit
+// 3. If the number of 1s is greater than the half of the array length, we set that bit to 1
+// 4. We return the number
+function solution2(array: number[]): number {
+  let answer = 0;
+  for (let currentBit = 0; currentBit < 32; currentBit++) {
+    let currentBitValue = 1 << currentBit;
+    let onesCount = 0;
+    for (let num of array) {
+      if (num & currentBitValue) onesCount++;
+    }
+
+    if (onesCount > array.length / 2) answer |= currentBitValue;
+  }
+
+  return answer;
+}
+
+// Bitwise v2 approach
+// Complexity (worst-case): time O(n) | space O(1)
+function solution3(array: number[]): number {
+  let answer = 0;
+  for (let currentBit = 0; currentBit < 32; currentBit++) {
+    let count = 0;
+    for (let num of array) {
+      if ((num >> currentBit) & 1) count++;
+    }
+
+    if (count > array.length / 2) answer |= 1 << currentBit;
   }
 
   return answer;
