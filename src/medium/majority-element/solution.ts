98c275f79768868775ec3083258c55aa557d2bd9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-11 18:07:26 -0300

    refactor(majority-element): my solution w/ hint 1

--- src/medium/majority-element/solution.ts
+++ src/medium/majority-element/solution.ts
@@ -7,20 +7,7 @@ function majorityElement(array: number[]): number {
 // Complexity (worst-case): time O(nlogn) | space O(1)
 function mySolution1(array: number[]): number {
   array.sort((a, b) => a - b);
-  let currMajority = array[0];
-  let maxCount = 0;
-  let currCount = 1;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === array[i + 1]) currCount++;
-    else {
-      if (currCount > maxCount) {
-        currMajority = array[i];
-        maxCount = currCount;
-      }
-      currCount = 1;
-    }
-  }
-  return currMajority;
+  return array[Math.floor(array.length/2)];
 }
 
 export default majorityElement;
