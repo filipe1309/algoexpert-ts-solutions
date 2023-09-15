1b684e5fbdb10cde06396c2bed05c1a511dc1464
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-15 19:23:42 -0300

    feat(find-kth-largest-value-in-bst): add 2nd solution

--- src/medium/find-kth-largest-value-in-bst/README.md
+++ src/medium/find-kth-largest-value-in-bst/README.md
@@ -37,3 +37,42 @@ k = 3
 ```
 17
 ```
+
+## Hints
+
+<details>
+<summary>Hint 1</summary>
+Make sure to consider the fact that the given tree is a Binary Search Tree—not
+just a regular Binary Tree. How does this fact help you solve the problem in a
+more optimal time complexity?
+</details>
+
+<details>
+<summary>Hint 2</summary>
+  The brute-force approach to this problem is to simply perform an in-order
+  traversal of this BST and to store all of its node' values in the order in
+  which they're visited. Since an in-order traversal of a BST visits the nodes
+  in ascending order, the `k`th value from the end of the traversal
+  order will be the `k`th largest value.
+</details>
+
+<details>
+<summary>Hint 3</summary>
+You can actually solve this problem in `O(h + k)` time, where
+`h` is the height of the tree. Rather than looking at the nodes in
+ascending order, you should look at them in descending order.
+</details>
+
+<details>
+<summary>Hint 4</summary>
+To solve this problem in `O(h + k)` time as mentioned in Hint #3,
+you need to perform a **reverse** in-order traversal. Since you'll be
+looking at nodes in descending order, you can simply return the
+`k`th visited node in the reverse in-order traversal.
+</details>
+
+<details>
+<summary>Optimal Space &amp; Time Complexity</summary>
+O(h + k) time | O(h) space - where h is the height of the tree and k is the
+input parameter
+</details>
--- src/medium/find-kth-largest-value-in-bst/solution.ts
+++ src/medium/find-kth-largest-value-in-bst/solution.ts
@@ -1,28 +1,58 @@
 // Test: make test t=find-kth-largest-value-in-bst
 function findKthLargestValueInBst(tree: BST, k: number): number {
-  return solutions.mySolution1(tree, k) as number;
+  return solutions.solution2(tree, k) as number;
 }
 
 const solutions = {
-  mySolution1, // time O(??) | space O(??)
-  // solution1 // time O(??) | space O(??)
+  mySolution1, // time O(n) | space O(n)
+  solution2    // time O(h + k) | space O(h)
   // solution2 // time O(??) | space O(??)
 };
 
 // Recursive In Order Traverse Reverse approach
 // Complexity (worst-case): time O(n) | space O(n)
+// where h is the height of the tree and
+// k is the input parameter
 function mySolution1(tree: BST, k: number): number {
   let array: number[] = [];
-  inOrderTraverseReverse(tree, array)
+  inOrderTraverseReverse(tree, k, array)
   return array[k - 1];
 }
 
-function inOrderTraverseReverse(tree: BST | null, array: number[]) {
-  if (!tree) return;
-  inOrderTraverseReverse(tree.right, array);
+function inOrderTraverseReverse(tree: BST | null, k: number, array: number[]) {
+  if (!tree || array.length === k) return;
+  inOrderTraverseReverse(tree.right, k, array);
   array.push(tree.value);
-  inOrderTraverseReverse(tree.left, array);
+  inOrderTraverseReverse(tree.left, k, array);
   return array;
+}
+
+class TreeInfo {
+  totalVisited: number;
+  lastVisited: number;
+
+  constructor(totalVisited: number, lastVisited: number) {
+    this.totalVisited = totalVisited;
+    this.lastVisited = lastVisited;
+  }
+}
+
+// Recursive In Order Traverse Reverse Optimized approach
+// Complexity (worst-case): time O(h + k) | space O(h)
+function solution2(tree: BST, k: number): number {
+  let treeInfo = new TreeInfo(0, -1);
+  inOrderTraverseReverseSolution2(tree, k, treeInfo)
+  return treeInfo.lastVisited;
+}
+
+function inOrderTraverseReverseSolution2(tree: BST | null, k: number, treeInfo: TreeInfo) {
+  if (!tree || treeInfo.totalVisited >= k) return;
+  inOrderTraverseReverseSolution2(tree.right, k, treeInfo);
+  if (treeInfo.totalVisited < k) {
+    treeInfo.totalVisited++;
+    treeInfo.lastVisited = tree.value
+    inOrderTraverseReverseSolution2(tree.left, k, treeInfo);
+  }
 }
 
 export default findKthLargestValueInBst;
