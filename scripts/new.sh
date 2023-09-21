bfb6e0263388505e8d6ca7cc99f1f09202c677b3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-21 20:15:26 -0300

    refactor(new): move temple content to create function

--- scripts/new.sh
+++ scripts/new.sh
@@ -15,24 +15,24 @@ do
   value=$(echo $argument | cut -f2 -d=)
 
   case "$key" in
-    "name")        NAME_TYPED="$value" ;;
+    "name")        NAME_SNAKE="$value" ;;
     "level")       level="$value" ;;
     "category")    category="$value" ;;
     *)
   esac
 done
-# read challenge name if not provided ${NAME_TYPED}
-if [ -z ${NAME_TYPED} ]; then 
+# read challenge name if not provided ${NAME_SNAKE}
+if [ -z ${NAME_SNAKE} ]; then 
   echo -e "Type challenge ${BOLD}name${RESET} in ${BOLD}snake-case${RESET} ${GRAY_DARKER}(ex: valid-subsequence)${RESET}";
   echo -e "${BOLD}${ITALIC}OR${RESET}";
   echo -e "Paste challenge ${BOLD}URL${RESET} ${GRAY_DARKER}(ex: https://www.algoexpert.io/questions/valid-subsequence)${RESET}";
-  read -p "👉 " NAME_TYPED
+  read -p "👉 " NAME_SNAKE
 fi
 # if challenge name is empty, exit
-if [ -z ${NAME_TYPED} ]; then echo "❌ Please provide a challenge name"; exit 1; fi
+if [ -z ${NAME_SNAKE} ]; then echo "❌ Please provide a challenge name"; exit 1; fi
 # if name is a url instead of a name, extract name from url
-if [[ ${NAME_TYPED} == *"https://www.algoexpert.io/questions/"* ]]; then
-  NAME_TYPED=$(echo ${NAME_TYPED} | sed 's/https:\/\/www.algoexpert.io\/questions\///g')
+if [[ ${NAME_SNAKE} == *"https://www.algoexpert.io/questions/"* ]]; then
+  NAME_SNAKE=$(echo ${NAME_SNAKE} | sed 's/https:\/\/www.algoexpert.io\/questions\///g')
 fi
 # read challenge level if not provided ${level}
 if [ -z ${level} ]; then 
@@ -48,7 +48,7 @@ fi
 if [ -z ${level} ]; then echo "❌ Please provide a level (easy, medium, hard, very-hard)"; exit 1; fi
 
 # Confirm with Yy or Enter
-echo -e "👉 You are about to create a new challenge ${GREEN}${BOLD}${NAME_TYPED}${RESET} in ${GREEN}${BOLD}${level}${RESET} level."
+echo -e "👉 You are about to create a new challenge ${GREEN}${BOLD}${NAME_SNAKE}${RESET} in ${GREEN}${BOLD}${level}${RESET} level."
 echo -e "👉 Is this correct? (Y/n)"
 read -p "👉 " confirm
 if [[ ${confirm} != "Y" && ${confirm} != "y" && ${confirm} != "" ]]; then echo -e "${RED}${BOLD}❌ Aborted!${RESET}"; exit 1; fi
@@ -56,8 +56,8 @@ echo -e "✅ ${GREEN}${BOLD}Confirmed!${RESET}"
 
 echo ""
 
-# "📝 Creating new challenge ${NAME_TYPED} in ${level} level..."
-echo -e "📝 Creating new challenge ${GREEN}${BOLD}${NAME_TYPED}${RESET} in ${GREEN}${BOLD}${level}${RESET} level..."
+# "📝 Creating new challenge ${NAME_SNAKE} in ${level} level..."
+echo -e "📝 Creating new challenge ${GREEN}${BOLD}${NAME_SNAKE}${RESET} in ${GREEN}${BOLD}${level}${RESET} level..."
 
 # Create directory
 # if level directory does not exist, create it
@@ -65,14 +65,14 @@ if [ ! -d "src/${level}" ]; then
   echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}${RESET} directory..."
   mkdir src/${level}
 fi
-echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_TYPED}${RESET} directory..."
-mkdir src/${level}/${NAME_TYPED}
+echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_SNAKE}${RESET} directory..."
+mkdir src/${level}/${NAME_SNAKE}
 
 # Create README file
-echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_TYPED}/README.md${RESET} file..."
-touch src/${level}/${NAME_TYPED}/README.md
+echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_SNAKE}/README.md${RESET} file..."
+touch src/${level}/${NAME_SNAKE}/README.md
 # replace - with empty space
-NAME=$(echo ${NAME_TYPED} | sed 's/-/ /g')
+NAME=$(echo ${NAME_SNAKE} | sed 's/-/ /g')
 # capitalize first letter of each word of NAME, ex: valid subsequence => Valid Subsequence
 NAME=$(echo ${NAME} | perl -pe 's/(\w+)/\u$1/g')
 # select category if not provided ${cat}
@@ -101,38 +101,36 @@ LEVEL=$(echo ${level} | perl -pe 's/(\w+)/\u$1/g')
 # capitalize category name
 CATEGORY=$(echo ${category} | perl -pe 's/(\w+)/\u$1/g')
 echo "# ${NAME}
-> Source: https://www.algoexpert.io/questions/${NAME_TYPED}  
+> Source: https://www.algoexpert.io/questions/${NAME_SNAKE}  
 > Difficulty: ${LEVEL}  
 > Category: ${CATEGORY}
 ---
-" >> src/${level}/${NAME_TYPED}/README.md
+" >> src/${level}/${NAME_SNAKE}/README.md
 
 
 create_challenge_file() {
   local level=$1
-  local NAME_TYPED=$2
-  local filename=$3
-  local content=$4
-  echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_TYPED}/${filename}${RESET} file..."
-  touch src/${level}/${NAME_TYPED}/${filename}
-  echo "$content" >> src/${level}/${NAME_TYPED}/${filename}
+  local NAME_SNAKE=$2
+  local NAME=$3
+  local filename=$4
+  local content=$5
+  content=$(export NAME=${NAME} && cat scripts/templates/${filename} | envsubst)
+  echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_SNAKE}/${filename}${RESET} file..."
+  touch src/${level}/${NAME_SNAKE}/${filename}
+  echo "$content" >> src/${level}/${NAME_SNAKE}/${filename}
 }
 
 
 # Create solution file
-CAMEL=$(echo ${NAME_TYPED} | perl -pe 's/(^|-)(\w)/\u$2/g' | perl -nE 'say lcfirst')
-content=$(export NAME=${CAMEL} && cat scripts/templates/solution-0.ts | envsubst)
-create_challenge_file ${level} ${NAME_TYPED} "solution-0.ts" "$content"
+CAMEL=$(echo ${NAME_SNAKE} | perl -pe 's/(^|-)(\w)/\u$2/g' | perl -nE 'say lcfirst')
+create_challenge_file ${level} ${NAME_SNAKE} ${CAMEL} "solution-0.ts" "$content"
 
 
 # Create test file
-# read file from template and replace variables
-content=$(export NAME=${NAME_TYPED} && cat scripts/templates/solution.spec.ts | envsubst)
-create_challenge_file ${level} ${NAME_TYPED} "solution.spec.ts" "$content"
+create_challenge_file ${level} ${NAME_SNAKE} ${NAME_SNAKE} "solution.spec.ts" "$content"
 
 # Create test cases file
-content=$(cat scripts/templates/cases.ts | envsubst)
-create_challenge_file ${level} ${NAME_TYPED} "cases.ts" "$content"
+create_challenge_file ${level} ${NAME_SNAKE} ${NAME_SNAKE} "cases.ts" "$content"
 
 echo -e "✅ ${GREEN}${BOLD}Done!${RESET}"
 
