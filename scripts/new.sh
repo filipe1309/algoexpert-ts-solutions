b5d16fd7a01334ebec17ec15419e3b68a323a14a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-09-22 21:33:14 -0300

    feat(new): create README from template

--- scripts/new.sh
+++ scripts/new.sh
@@ -16,16 +16,16 @@ do
 
   case "$key" in
     "name")        NAME_SNAKE="$value" ;;
-    "level")       level="$value" ;;
-    "category")    category="$value" ;;
+    "level")       LEVEL_LOWERCASE="$value" ;;
+    "category")    CATEGORY="$value" ;;
     *)
   esac
 done
 # read challenge name if not provided ${NAME_SNAKE}
 if [ -z ${NAME_SNAKE} ]; then 
-  echo -e "Type challenge ${BOLD}name${RESET} in ${BOLD}snake-case${RESET} ${GRAY_DARKER}(ex: valid-subsequence)${RESET}";
-  echo -e "${BOLD}${ITALIC}OR${RESET}";
-  echo -e "Paste challenge ${BOLD}URL${RESET} ${GRAY_DARKER}(ex: https://www.algoexpert.io/questions/valid-subsequence)${RESET}";
+  echo -e "Type challenge ${BOLD}name${NC} in ${BOLD}snake-case${NC} ${GRAY_DARKER}(ex: valid-subsequence)${NC}";
+  echo -e "${BOLD}${ITALIC}OR${NC}";
+  echo -e "Paste challenge ${BOLD}URL${NC} ${GRAY_DARKER}(ex: https://www.algoexpert.io/questions/valid-subsequence)${NC}";
   read -p "👉 " NAME_SNAKE
 fi
 # if challenge name is empty, exit
@@ -34,103 +34,96 @@ if [ -z ${NAME_SNAKE} ]; then echo "❌ Please provide a challenge name"; exit 1
 if [[ ${NAME_SNAKE} == *"https://www.algoexpert.io/questions/"* ]]; then
   NAME_SNAKE=$(echo ${NAME_SNAKE} | sed 's/https:\/\/www.algoexpert.io\/questions\///g')
 fi
-# read challenge level if not provided ${level}
-if [ -z ${level} ]; then 
+# read challenge level if not provided ${LEVEL_LOWERCASE}
+if [ -z ${LEVEL_LOWERCASE} ]; then 
   echo "Challenge level:"
   case `select_opt "😎 easy" "😅 medium" "😰 hard" "😱 very-hard"` in
-      0) level="easy";;
-      1) level="medium";;
-      2) level="hard";;
-      3) level="very-hard";;
+      0) LEVEL_LOWERCASE="easy";;
+      1) LEVEL_LOWERCASE="medium";;
+      2) LEVEL_LOWERCASE="hard";;
+      3) LEVEL_LOWERCASE="very-hard";;
   esac
 fi
 # if challenge level is empty, exit
-if [ -z ${level} ]; then echo "❌ Please provide a level (easy, medium, hard, very-hard)"; exit 1; fi
+if [ -z ${LEVEL_LOWERCASE} ]; then echo "❌ Please provide a level (easy, medium, hard, very-hard)"; exit 1; fi
 
 # Confirm with Yy or Enter
-echo -e "👉 You are about to create a new challenge ${GREEN}${BOLD}${NAME_SNAKE}${RESET} in ${GREEN}${BOLD}${level}${RESET} level."
+echo -e "👉 You are about to create a new challenge ${GREEN}${BOLD}${NAME_SNAKE}${NC} in ${GREEN}${BOLD}${LEVEL_LOWERCASE}${NC} level."
 echo -e "👉 Is this correct? (Y/n)"
 read -p "👉 " confirm
-if [[ ${confirm} != "Y" && ${confirm} != "y" && ${confirm} != "" ]]; then echo -e "${RED}${BOLD}❌ Aborted!${RESET}"; exit 1; fi
-echo -e "✅ ${GREEN}${BOLD}Confirmed!${RESET}"
+if [[ ${confirm} != "Y" && ${confirm} != "y" && ${confirm} != "" ]]; then echo -e "${RED}${BOLD}❌ Aborted!${NC}"; exit 1; fi
+echo -e "✅ ${GREEN}${BOLD}Confirmed!${NC}"
 
 echo ""
 
-# "📝 Creating new challenge ${NAME_SNAKE} in ${level} level..."
-echo -e "📝 Creating new challenge ${GREEN}${BOLD}${NAME_SNAKE}${RESET} in ${GREEN}${BOLD}${level}${RESET} level..."
+# "📝 Creating new challenge ${NAME_SNAKE} in ${LEVEL_LOWERCASE} level..."
+echo -e "📝 Creating new challenge ${GREEN}${BOLD}${NAME_SNAKE}${NC} in ${GREEN}${BOLD}${LEVEL_LOWERCASE}${NC} level..."
 
 # Create directory
 # if level directory does not exist, create it
-if [ ! -d "src/${level}" ]; then
-  echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}${RESET} directory..."
-  mkdir src/${level}
+if [ ! -d "src/${LEVEL_LOWERCASE}" ]; then
+  echo -e " 👉 Creating ${GRAY}${BOLD}src/${LEVEL_LOWERCASE}${NC} directory..."
+  mkdir src/${LEVEL_LOWERCASE}
 fi
-echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_SNAKE}${RESET} directory..."
-mkdir src/${level}/${NAME_SNAKE}
+echo -e " 👉 Creating ${GRAY}${BOLD}src/${LEVEL_LOWERCASE}/${NAME_SNAKE}${NC} directory..."
+mkdir src/${LEVEL_LOWERCASE}/${NAME_SNAKE}
+
+create_challenge_file() {
+  local LEVEL=$1
+  local NAME_SNAKE=$2
+  local NAME=$3
+  local FILENAME=$4
+  local CATEGORY=$5
+  local CONTENT=$6
+  # if CONTENT is empty, read CONTENT from template file
+  if [[ -z ${CONTENT} ]]; then
+    # capitalize level name
+    LEVEL_CAP=$(echo ${LEVEL} | perl -pe 's/(\w+)/\u$1/g')
+    CONTENT=$(export NAME=${NAME} && export NAME_SNAKE=${NAME_SNAKE} && export LEVEL=${LEVEL_CAP} && export CATEGORY=${CATEGORY} && cat scripts/templates/${FILENAME} | envsubst)
+  fi
+  echo -e " 👉 Creating ${GRAY}${BOLD}src/${LEVEL}/${NAME_SNAKE}/${FILENAME}${NC} file..."
+  touch src/${LEVEL}/${NAME_SNAKE}/${FILENAME}
+  echo "$CONTENT" >> src/${LEVEL}/${NAME_SNAKE}/${FILENAME}
+}
 
 # Create README file
-echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_SNAKE}/README.md${RESET} file..."
-touch src/${level}/${NAME_SNAKE}/README.md
+# select category if not provided ${cat}
+if [[ -z ${CATEGORY} ]]; then
+  echo "Category:"
+  case `select_opt "Arrays" "Binary Trees" "Binary Search Trees" "Dynamic Programming" "Famous Algorithms" "Graphs" "Greedy Algorithms" "Heaps" "Linked Lists" "Recursion" "Searching" "Sorting" "Stacks" "Strings" "Tries"` in
+      0) CATEGORY="Arrays";;
+      1) CATEGORY="Binary Trees";;
+      2) CATEGORY="Binary Search Trees";;
+      3) CATEGORY="Dynamic Programming";;
+      4) CATEGORY="Famous Algorithms";;
+      5) CATEGORY="Graphs";;
+      6) CATEGORY="Greedy Algorithms";;
+      7) CATEGORY="Heaps";;
+      8) CATEGORY="Linked Lists";;
+      9) CATEGORY="Recursion";;
+      10) CATEGORY="Searching";;
+      11) CATEGORY="Sorting";;
+      12) CATEGORY="Stacks";;
+      13) CATEGORY="Strings";;
+      14) CATEGORY="Tries";;
+  esac
+fi
+CATEGORY_CAP=$(echo ${CATEGORY} | perl -pe 's/(\w+)/\u$1/g')
 # replace - with empty space
 NAME=$(echo ${NAME_SNAKE} | sed 's/-/ /g')
 # capitalize first letter of each word of NAME, ex: valid subsequence => Valid Subsequence
-NAME=$(echo ${NAME} | perl -pe 's/(\w+)/\u$1/g')
-# select category if not provided ${cat}
-if [[ -z ${category} ]]; then
-  echo "Category:"
-  case `select_opt "Arrays" "Binary Trees" "Binary Search Trees" "Dynamic Programming" "Famous Algorithms" "Graphs" "Greedy Algorithms" "Heaps" "Linked Lists" "Recursion" "Searching" "Sorting" "Stacks" "Strings" "Tries"` in
-      0) category="Arrays";;
-      1) category="Binary Trees";;
-      2) category="Binary Search Trees";;
-      3) category="Dynamic Programming";;
-      4) category="Famous Algorithms";;
-      5) category="Graphs";;
-      6) category="Greedy Algorithms";;
-      7) category="Heaps";;
-      8) category="Linked Lists";;
-      9) category="Recursion";;
-      10) category="Searching";;
-      11) category="Sorting";;
-      12) category="Stacks";;
-      13) category="Strings";;
-      14) category="Tries";;
-  esac
-fi
-# capitalize level name
-LEVEL=$(echo ${level} | perl -pe 's/(\w+)/\u$1/g')
-# capitalize category name
-CATEGORY=$(echo ${category} | perl -pe 's/(\w+)/\u$1/g')
-echo "# ${NAME}
-> Source: https://www.algoexpert.io/questions/${NAME_SNAKE}  
-> Difficulty: ${LEVEL}  
-> Category: ${CATEGORY}
----
-" >> src/${level}/${NAME_SNAKE}/README.md
-
-
-create_challenge_file() {
-  local level=$1
-  local NAME_SNAKE=$2
-  local NAME=$3
-  local filename=$4
-  local content=$5
-  content=$(export NAME=${NAME} && cat scripts/templates/${filename} | envsubst)
-  echo -e " 👉 Creating ${GRAY}${BOLD}src/${level}/${NAME_SNAKE}/${filename}${RESET} file..."
-  touch src/${level}/${NAME_SNAKE}/${filename}
-  echo "$content" >> src/${level}/${NAME_SNAKE}/${filename}
-}
-
+NAME_CAP=$(echo ${NAME} | perl -pe 's/(\w+)/\u$1/g')
+create_challenge_file ${LEVEL_LOWERCASE} ${NAME_SNAKE} "${NAME_CAP}" "README.md" "${CATEGORY_CAP}"
 
 # Create solution file
-CAMEL=$(echo ${NAME_SNAKE} | perl -pe 's/(^|-)(\w)/\u$2/g' | perl -nE 'say lcfirst')
-create_challenge_file ${level} ${NAME_SNAKE} ${CAMEL} "solution-0.ts" "$content"
-
+NAME_CAMEL=$(echo ${NAME_SNAKE} | perl -pe 's/(^|-)(\w)/\u$2/g' | perl -nE 'say lcfirst')
+create_challenge_file ${LEVEL_LOWERCASE} ${NAME_SNAKE} ${NAME_CAMEL} "solution-0.ts"
 
 # Create test file
-create_challenge_file ${level} ${NAME_SNAKE} ${NAME_SNAKE} "solution.spec.ts" "$content"
+create_challenge_file ${LEVEL_LOWERCASE} ${NAME_SNAKE} ${NAME_SNAKE} "solution.spec.ts"
 
 # Create test cases file
-create_challenge_file ${level} ${NAME_SNAKE} ${NAME_SNAKE} "cases.ts" "$content"
+create_challenge_file ${LEVEL_LOWERCASE} ${NAME_SNAKE} ${NAME_SNAKE} "cases.ts"
 
-echo -e "✅ ${GREEN}${BOLD}Done!${RESET}"
+echo -e "✅ ${GREEN}${BOLD}Done!${NC}"
 
---
+++ scripts/templates/README.md
@@ -0,0 +1,17 @@
+# ${NAME}
+> Source: https://www.algoexpert.io/questions/${NAME_SNAKE}  
+> Difficulty: ${LEVEL}  
+> Category: ${CATEGORY}
+---
+
+
+
+**Sample Input**
+```ts
+
+```
+
+**Sample Output**
+```ts
+
+```
