92a57e7e8a0992f61a242fd606954a768f514678
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-10-18 22:51:58 -0300

    refactor(valid-starting-city): solution 0

--- src/medium/valid-starting-city/solutions/solution-0.ts
+++ src/medium/valid-starting-city/solutions/solution-0.ts
@@ -1,18 +1,16 @@
 // BF approach
 // Complexity (worst-case): O(n^2) time | O(1) space
 function validStartingCity(distances: number[], fuel: number[], mpg: number) {
-  let validStartingCityIdx = 0;
-  fuel = fuel.map((f) => f * mpg);
-  for (let i = 0; i < distances.length; i++) {
-    let currFuel = 0;
-    for (let j = 0; j < distances.length; j++) {
-      let pos = (i + j) % distances.length;
-      currFuel += fuel[pos] - distances[pos];
-      if (currFuel < 0) break;
-      if (currFuel >= 0 && j === distances.length - 1) return i;
+  for (let startCityIdx = 0; startCityIdx < distances.length; startCityIdx++) {
+    let currMilesAvailable = 0;
+    for (let currCityIdx = 0; currCityIdx < distances.length; currCityIdx++) {
+      let pos = (startCityIdx + currCityIdx) % distances.length;
+      currMilesAvailable += fuel[pos]*mpg - distances[pos];
+      if (currMilesAvailable < 0) break;
     }
+    if (currMilesAvailable >= 0) return startCityIdx;
   }
-  return validStartingCityIdx;
+  return -1;
 }
 
 export { validStartingCity as solution0 };
