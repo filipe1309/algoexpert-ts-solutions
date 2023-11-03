b7e8b30155b6640d382f1b29fd8f4950b9ba6a41
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-11-03 17:04:41 -0300

    refactor(staircase-traversal):  to use Tree Traversal && Array Of Sums approach

--- src/medium/staircase-traversal/solutions/solution-0.ts
+++ src/medium/staircase-traversal/solutions/solution-0.ts
@@ -1,22 +1,23 @@
-// ?? approach
+// Tree Traversal && Array Of Sums approach
 // Complexity (worst-case): O(n^k) time | O(n) space
 // where n = height, k = maxSteps
 function staircaseTraversal(height: number, maxSteps: number) {
-  const total: number[] = [];
-  staircaseTraversalHelper([], height, maxSteps, total)
-  return total.length;
+  return sTHelper([], height, maxSteps)
 }
 
-function staircaseTraversalHelper(currSum: number[], height: number, maxSteps: number, total: number[]) {
+function sTHelper(currSum: number[], height: number, maxSteps: number) {
   const currTotal = currSum.reduce((p, v) => p + v, 0)
-  if (currTotal === height) { total.push(1); return; }
-  else if (currTotal > height) return;
-
+  if (currTotal === height) return 1;
+  if (currTotal > height) return 0;
+  
+  let sum = 0;
   for (let i = 1; i <= maxSteps; i++) {
     currSum.push(i);
-    staircaseTraversalHelper(currSum, height, maxSteps, total);
+    sum += sTHelper(currSum, height, maxSteps);
     currSum.pop();
   }
+
+  return sum;
 }
 
 export { staircaseTraversal as solution0 };
